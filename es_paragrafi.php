d882eee42d452a91aef942d8b4cf69e92c50f50c
Benncio09 <lorebencini13@outlook.com>
2024-10-03 09:31:48 +0200

    esercizio finito e funzionante

---
+++ es_paragrafi.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+        $size = 0;
+        $num = 5;
+
+
+
+        for ($i = 0; $i <= $num; $i++){
+            $size = $size + 2;
+            echo "<p style= 'font-size: {$size}px;'> Lorem ipsum dolor sit amet. Aut fugit nihil a temporibus quod aut suscipit aliquam est dolorem dolorum ut necessitatibus quos. Et libero unde 33 fugit eveniet et perspiciatis voluptas qui consequatur voluptas sit velit libero. Sed dicta consectetur est quisquam dolore ea sapiente nesciunt ut modi dolor sit quia deleniti et eius ipsum. Aut labore officia et autem molestiae ab corporis adipisci et natus voluptatem qui galisum voluptas aut porro consequatur et eveniet labore? </p>";
+        }
+
+
+
+
+    ?>
+</body>
+</html>
